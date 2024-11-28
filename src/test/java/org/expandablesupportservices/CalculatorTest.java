becf193d3e8d63c86264a2eb3d7fd53bd41a8e1a
MASTER\allfo <kamranali.says@gmail.com>
2024-11-28 06:01:08 +0000

    Adding Parameterize test method.

--- src/test/java/org/expandablesupportservices/CalculatorTest.java
+++ src/test/java/org/expandablesupportservices/CalculatorTest.java
@@ -3,12 +3,17 @@ package org.expandablesupportservices;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 public class CalculatorTest {
@@ -44,35 +49,40 @@ public class CalculatorTest {
 	
 	@Test
 	void test_addition()
-	{
-		
+	{	
 		assertEquals(4, calculator.addition(2, 2));
 	}
 
-	@Test
-	void test_subtraction()
+	@ParameterizedTest
+	@MethodSource("subtractionInputParameters")
+	void test_subtraction(int a , int b ,int c)
 	{
-		
-		assertEquals(2, calculator.subtraction(4, 2));
+		assertEquals(c, calculator.subtraction(a, b));
 	}
 
 	
 	@Test
 	void test_multiplication()
-	{
-		
-		assertEquals(16, calculator.multiplication(4,4) ,()-> "it does not produce expected output");
-		
+	{	
+		assertEquals(16, calculator.multiplication(4,4) ,()-> "it does not produce expected output");	
 	}
 	
 	@DisplayName(" division by 0 ")
 	@Test
 	void test_division()
 	{
-		
-		
-		assertThrows(ArithmeticException.class, ()-> calculator.division(2, 0) );
+		assertThrows(ArithmeticException.class, ()-> { calculator.division(2, 0); } );
 	}
 	
-	 
+
+	private static Stream<Arguments>  subtractionInputParameters()
+	{
+		return Stream.of(
+				Arguments.of(10,9,1),
+				Arguments.of(25,23,2),
+				Arguments.of(12,7,4)
+				);
+	}
+	
+	
 }
