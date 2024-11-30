a0739c9d1c39e55754b5b3a01418a7c67ac734a6
MASTER\allfo <kamranali.says@gmail.com>
2024-11-30 11:40:11 +0000

    Test with @TestMethodOrder annoattion

---
+++ src/test/java/org/expandablesupportservices/MethodTestsWithRandomOrder.java
@@ -0,0 +1,32 @@
+package org.expandablesupportservices;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodTestsWithRandomOrder {
+	
+	
+	
+	@Test
+	void testMethodA()
+	{ assertEquals("A", "A");}
+
+	@Test
+	void testMethodB()
+	{ assertEquals("B", "B");}
+	
+	@Test
+	void testMethodC()
+	{ assertEquals("C", "C");}
+	
+	@Test
+	void testMethodD()
+	{ assertEquals("D", "D");}
+	
+	
+	
+}
