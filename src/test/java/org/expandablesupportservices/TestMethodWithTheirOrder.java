940d36975295cb94f342ca382e66c17edd775f4d
MASTER\allfo <kamranali.says@gmail.com>
2024-12-01 09:24:55 +0000

    Customize the Test instance life cyle to Per Class.

--- src/test/java/org/expandablesupportservices/TestMethodWithTheirOrder.java
+++ src/test/java/org/expandablesupportservices/TestMethodWithTheirOrder.java
@@ -2,33 +2,55 @@ package org.expandablesupportservices;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestMethodWithTheirOrder {
 
+	StringBuilder stringBuilder=new StringBuilder("");
+	
+	@AfterAll
+	void afterAllMethod() {
+		
+		System.out.println(stringBuilder);
+		
+	}
+	
 	@Order(1)
 	@Test
 	void testMethodD()
-	{ assertEquals("D", "D");}
+	{ 
+		stringBuilder.append("1");
+		assertEquals("D", "D");}
 	
 	@Order(2)
 	@Test
 	void testMethodA()
-	{ assertEquals("A", "A");}
+	{ 
+		stringBuilder.append("2");
+		assertEquals("A", "A");}
 	
 	@Order(3)
 	@Test
 	void testMethodC()
-	{ assertEquals("C", "C");}
+	{ 
+		stringBuilder.append("3");
+		assertEquals("C", "C");}
 
 	@Order(4)
 	@Test
 	void testMethodB()
-	{ assertEquals("B", "B");}
+	{ 
+		stringBuilder.append("4");
+		assertEquals("B", "B");
+		
+	}
 	
 
 
